08e7cd6ffef946d0dbae747330f9d86f010acb02
RetroButterbacke <94745658+RetroButterbacke@users.noreply.github.com>
2024-03-29 00:16:28 +0100

    Add files via upload

---
+++ graphics_h/__init__.py
@@ -0,0 +1,1 @@
+from .graphics_h import *
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='graphics_h',
+    version='1.0',
+    author='RetroButterbacke',
+    author_email='butterbacke51@gmail.com',
+    description='A simple header for the pygame libary made for my informatic course in school',
+    packages=['graphics_h'],
+    python_requires='>=3.7',
+    zip_safe=False
+)
