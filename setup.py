021811b46e6c400c22f750e1246db747a615b372
RetroButterbacke <94745658+RetroButterbacke@users.noreply.github.com>
2024-03-31 20:52:10 +0100

    Add files via upload

--- setup.py
+++ setup.py
@@ -5,8 +5,7 @@ setup(
     version='1.0',
     author='RetroButterbacke',
     author_email='butterbacke51@gmail.com',
-    description='A simple compilation of headers for the pygame libary made for my informatic course in school',
+    description='A simple compilation of headers for the pygame libary made for my computer sience course in school',
     packages=['graphics_pg', 'graphics_pg_old', 'graphics_pg_plus', 'graphics_pg_3d'],
-    python_requires='>=3.7',
-    zip_safe=False
+    python_requires='>=3.7'
 )
