b81a8af70bd9834ba57410c933c776101c481147
RetroButterbacke <94745658+RetroButterbacke@users.noreply.github.com>
2025-07-01 09:51:48 +0200

    Update graphics_pg.py

--- graphics_pg/graphics_pg.py
+++ graphics_pg/graphics_pg.py
@@ -467,6 +467,12 @@ class Window:
     
     def getLiteralFPS(self) -> float:
         return self.fps
+
+    def getAt(self, x: int, y: int) -> 'rgba':
+        return self.surf.get_at([x,y])
+
+    def setAt(self, x: int, y: int, col: rgb | rgba) -> None:
+        self.surf.set_at([x, y], [col.red, col.green, col.blue, col.alpha if type(col) is rgba else 255]
     
     def setFrameRate(self, rate: int) -> None:
         self.framerate = rate
